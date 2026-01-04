4aead51fcac77c741b310edf5da9bc593dc1b941
ohmybiino <davenarzobal@gmail.com>
2026-01-04 22:05:34 +0800

    Configure Entity Relationship: configure the relationship of User Table and Notes Table

--- backend/Jotly.api/Jotly.api/Data/AppDbContext.cs
+++ backend/Jotly.api/Jotly.api/Data/AppDbContext.cs
@@ -1,6 +1,26 @@
-﻿namespace Jotly.api.Data
+﻿using Jotly.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jotly.api.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<User> Users { get; set; } = default!;
+        public DbSet<Notes> Notes { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Notes)
+                .WithOne(u => u.User)
+                .HasForeignKey(u => u.UserId);
+        }
     }
 }
