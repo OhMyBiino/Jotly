c1fe471b5500b46eda422421240aad18196864bf
ohmybiino <davenarzobal@gmail.com>
2026-01-04 21:16:31 +0800

    Add Entities

---
+++ backend/Jotly.api/Jotly.api/Models/Notes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jotly.api.Models
+{
+    public class Notes
+    {
+        [Key]
+        public int NoteId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime Created_At { get; set; } = DateTime.Now;
+
+        public int UserId { get; set; }
+    }
+}
