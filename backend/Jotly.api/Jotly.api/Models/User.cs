cdfed3e87c2a6d30c631a1a7ff85420f278cf4bb
ohmybiino <davenarzobal@gmail.com>
2026-01-05 12:24:11 +0800

    Add Key Annotation: add key annotation for User entity

--- backend/Jotly.api/Jotly.api/Models/User.cs
+++ backend/Jotly.api/Jotly.api/Models/User.cs
@@ -4,6 +4,7 @@ namespace Jotly.api.Models
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
