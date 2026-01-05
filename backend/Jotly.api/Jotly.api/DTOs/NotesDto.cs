eae8d8cb221c691f5ed1be51354d9b337814dfff
ohmybiino <davenarzobal@gmail.com>
2026-01-05 13:18:25 +0800

    Add Entity DTO

---
+++ backend/Jotly.api/Jotly.api/DTOs/NotesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Jotly.api.DTOs
+{
+    public class NotesDto
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime Created_At { get; set; } 
+    }
+}
