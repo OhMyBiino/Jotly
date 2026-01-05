3692296e849adf4fe8bd34550fe22d9d42cb0362
ohmybiino <davenarzobal@gmail.com>
2026-01-05 15:28:55 +0800

    Add NoteService

---
+++ backend/Jotly.api/Jotly.api/Services/Notes/INoteService.cs
@@ -0,0 +1,13 @@
+﻿using Jotly.api.DTOs;
+
+namespace Jotly.api.Services.Notes
+{
+    public interface INoteService
+    {
+        Task<IEnumerable<NotesDto>> GetAllAsync();
+        Task<NotesDto> GetByIdAsync(int id);
+        Task AddAsync(NotesDto notesDto);
+        Task<NotesDto> UpdateAsync(NotesDto notesDto);
+        Task<NotesDto> DeleteAsync(int Id);
+    }
+}
