62abfcb0d6c6a981edee6b0b9961dde65476e063
ohmybiino <davenarzobal@gmail.com>
2026-01-07 19:42:23 +0800

    Add Request Dto

---
+++ backend/Jotly.api/Jotly.api/DTOs/CreateNoteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Jotly.api.DTOs
+{
+    public class CreateNoteDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public int UserId { get; set; }
+    }
+}
