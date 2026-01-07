804ce01bb51ed6981958fb637f38b2d303eb21a7
ohmybiino <davenarzobal@gmail.com>
2026-01-07 20:13:04 +0800

    Enhance UserDto Property

--- backend/Jotly.api/Jotly.api/DTOs/UserDto.cs
+++ backend/Jotly.api/Jotly.api/DTOs/UserDto.cs
@@ -4,6 +4,6 @@
     {
         public int UserId { get; set; }
         public string Email { get; set; } = string.Empty;
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
     }
 }
