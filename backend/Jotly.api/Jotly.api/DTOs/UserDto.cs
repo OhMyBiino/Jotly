9d2ce98bd167998bc6854791565c85ea0c87d9b5
ohmybiino <davenarzobal@gmail.com>
2026-01-07 19:59:42 +0800

    Add UserDto

---
+++ backend/Jotly.api/Jotly.api/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Jotly.api.DTOs
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Emai { get; set; } = string.Empty;
+        public string Username { get; set; }
+    }
+}
