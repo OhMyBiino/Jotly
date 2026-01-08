816193c0625fd80939025bd674c9992bbbecc916
ohmybiino <davenarzobal@gmail.com>
2026-01-08 18:48:34 +0800

    Add Request Dto for creating User entity

--- backend/Jotly.api/Jotly.api/Services/UserService/IUserService.cs
+++ backend/Jotly.api/Jotly.api/Services/UserService/IUserService.cs
@@ -4,7 +4,7 @@ namespace Jotly.api.Services.UserService
 {
     public interface IUserService
     {
-        Task<UserDto> CreateAsync(UserDto request);
+        Task<UserDto> CreateAsync(CreateUserDto request);
         Task<IEnumerable<UserDto>> GetAllAsync();
     }
 }
