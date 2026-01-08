03ec96442af91114b6bba7aa8e463b562010d552
ohmybiino <davenarzobal@gmail.com>
2026-01-08 19:25:21 +0800

    Add GetById operation and Enhance return type of CreateUser function

--- backend/Jotly.api/Jotly.api/Controllers/UsersController.cs
+++ backend/Jotly.api/Jotly.api/Controllers/UsersController.cs
@@ -14,13 +14,23 @@ namespace Jotly.api.Controllers
         {
             var user = await _userService.CreateAsync(request);
 
-            return Ok(user);
+            return CreatedAtAction(nameof(GetById), new { id = user.UserId }, user);
         }
 
         [HttpGet("getall")]
         public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUser() 
         {
             var result = await _userService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("getbyId-{Id:int}")]
+        public async Task<ActionResult<UserDto>> GetById([FromRoute] int Id) 
+        {
+            var result = await _userService.GetByIdAsync(Id);
+            if(result == null) 
+                return NotFound("User not found.");
+
             return Ok(result);
         }
     }
