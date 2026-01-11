018f4eb53ab4d2410d677d533ccf09e9b01d24a9
ohmybiino <davenarzobal@gmail.com>
2026-01-11 14:27:06 +0800

    Improve AuthResult Dto property naming

--- backend/Jotly.api/Jotly.api/DTOs/AuthResult.cs
+++ backend/Jotly.api/Jotly.api/DTOs/AuthResult.cs
@@ -8,7 +8,7 @@
         public string? Token { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime? ExpiresAt { get; set; }
-        public UserDto? user { get; set; }
+        public UserDto? User { get; set; }
         public List<string> Errors { get; set; } = new();
 
         #endregion Properties
