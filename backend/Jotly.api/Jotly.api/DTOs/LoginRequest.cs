dd4ff9ad51095880c84ccba17898d46e8e7ba3e1
ohmybiino <davenarzobal@gmail.com>
2026-01-11 21:49:17 +0800

    Add Login Request Dto

---
+++ backend/Jotly.api/Jotly.api/DTOs/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Jotly.api.DTOs
+{
+    public class LoginRequest
+    {
+        #region Properties
+
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        #endregion Properties
+    }
+}
