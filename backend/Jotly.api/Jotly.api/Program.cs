553213cd43d09c36d69cacf96ea112ec68741241
ohmybiino <davenarzobal@gmail.com>
2026-01-11 21:51:16 +0800

    Register Authentication and Token Service in DI Container

--- backend/Jotly.api/Jotly.api/Program.cs
+++ backend/Jotly.api/Jotly.api/Program.cs
@@ -1,7 +1,9 @@
 using Jotly.api.Data;
 using Jotly.api.Repositories.NotesRepo;
 using Jotly.api.Repositories.UserRepo;
+using Jotly.api.Services.AuthService;
 using Jotly.api.Services.NoteService;
+using Jotly.api.Services.TokenService;
 using Jotly.api.Services.UserService;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,6 +26,12 @@ builder.Services.AddScoped<INoteService, NoteService>();
 //Register User Service-Repository
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+//Register AuthService
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+//Register Token Service
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
